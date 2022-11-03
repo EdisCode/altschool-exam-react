466d4a1c9e29c9189fabaa346acc1a55b4f92719
Edikan Ekanem <cema4.ee@gmail.com>
2022-11-03 17:08:35 +0100

    firebase

---
+++ src/config/index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrDx2xB5Qjxf_cmdXcsHYCMNn6nK9dzP4",
+  authDomain: "altschool-exam-af66f.firebaseapp.com",
+  projectId: "altschool-exam-af66f",
+  storageBucket: "altschool-exam-af66f.appspot.com",
+  messagingSenderId: "727361954166",
+  appId: "1:727361954166:web:32a3c4fc2ec22f76725a57",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as Home } from "./Home";
-export { default as PageNotFound } from "./PageNotFound";
 export { default as Counter } from "./Counter";
+export { default as PageNotFound } from "./PageNotFound";
 export { default as FallBack } from "./FallBack";
